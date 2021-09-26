0d9fcb65d2915c5bf59d6f30f4e39afc53f38746
KamalDGRT <kamaldgrt@gmail.com>
2021-09-26 12:33:41 +0530

    mad: add exp9: email
    remove exp9
    
    mad: exp 9: email: attempt 2

---
+++ MAD/Ex_09_Email/app/src/main/java/com/example/ex09/Utils.java
@@ -0,0 +1,8 @@
+package com.example.ex09;
+
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "your email id here";
+    public static final String PASSWORD = "your app specific password here";
+}
