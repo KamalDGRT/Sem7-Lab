c2f2c0645933a165f7e59260e5aec7b053107644
KamalDGRT <kamaldgrt@gmail.com>
2021-09-27 11:37:30 +0530

    try exp10 again

--- MAD/Ex_09_Email/app/src/main/java/com/example/ex09/Utils.java
+++ MAD/Ex_09_Email/app/src/main/java/com/example/ex09/Utils.java
@@ -3,6 +3,6 @@ package com.example.ex09;
 public class Utils {
     // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
 
-    public static final  String EMAIL = "your email id here";
-    public static final String PASSWORD = "your app specific password here";
+    public static final  String EMAIL = "your email address";
+    public static final String PASSWORD = "your email password";
 }
